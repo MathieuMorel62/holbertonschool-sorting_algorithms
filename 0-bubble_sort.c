147ba6c976218fd3edbb51f4f9b3a39beeb50152
MathieuMorel62 <mormth@aol.com>
2022-12-16 07:11:36 -0800

    betty coding style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,28 +3,28 @@
 /**
  * bubble_sort - function that sorts an array of integers in ascending order
  * @array: array to sort
- * @size: number of element 
+ * @size: number of element
  */
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
+	size_t i, j;
+	int temp;
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                /* Swap the elements */
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				/* Swap the elements */
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 
-                /* Print the array after each swap */
-                print_array(array, size);
-            }
-        }
-    }
+				/* Print the array after each swap */
+				print_array(array, size);
+			}
+		}
+	}
 }

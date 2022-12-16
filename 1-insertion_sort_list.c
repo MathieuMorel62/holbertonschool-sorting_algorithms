6c165e2f76fa5aed934bd83b1b75286c0c563dd2
soOwasTaken <444deph12@gmail.com>
2022-12-16 11:27:45 +0000

    fixing 1-O, and now handle properly NULL pointer

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,32 +6,38 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = *list;
-	listint_t *temp = NULL;
-
 	if (list == NULL || *list == NULL)
 		return;
 
-	while (current != NULL)
+	else
 	{
-		temp = current;
-		while (temp != NULL && temp->prev != NULL && temp->n < temp->prev->n)
-		{
-			/* Swap nodes */
-			temp->prev->next = temp->next;
-			if (temp->next != NULL)
-				temp->next->prev = temp->prev;
-			temp->next = temp->prev;
-			temp->prev = temp->prev->prev;
-			temp->next->prev = temp;
-			if (temp->prev != NULL)
-				temp->prev->next = temp;
-			else
-				*list = temp;
+		listint_t *current = *list;
+		listint_t *temp = NULL;
 
-			/* Print list after each swap */
-			print_list(*list);
+		if (list == NULL || *list == NULL || current == NULL)
+			return;
+
+		while (current != NULL)
+		{
+			temp = current;
+			while (temp != NULL && temp->prev != NULL && temp->n < temp->prev->n)
+			{
+				/* Swap nodes */
+				temp->prev->next = temp->next;
+				if (temp->next != NULL)
+					temp->next->prev = temp->prev;
+				temp->next = temp->prev;
+				temp->prev = temp->prev->prev;
+				temp->next->prev = temp;
+				if (temp->prev != NULL)
+					temp->prev->next = temp;
+				else
+					*list = temp;
+
+				/* Print list after each swap */
+				print_list(*list);
+			}
+			current = current->next;
 		}
-		current = current->next;
 	}
 }

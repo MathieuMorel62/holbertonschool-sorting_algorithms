0c1d4e9631d1cf9f3b07cd7e998dcb2e15d729e6
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2022-12-17 12:29:05 +0100

    add function selection sort and O file

---
+++ 2-selection_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending order using the
+ *                  selection sort algorithm
+ * @array: the array to be sorted
+ * @size: the size of te array
+*/
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_index;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+			{
+				min_index = j;
+			}
+		}
+		if (min_index != i)
+		{
+			/* Swap array[i] and array[min_index] */
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+
+			/* Print the array after the swap */
+			for (j = 0; j < size; j++)
+			{
+				printf("%d ", array[j]);
+			}
+			printf("\n");
+		}
+	}
+}

213429c61bf32c0c37f63cc52483bbe1111c0126
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2022-12-17 12:41:26 +0100

    add condition and print array on the loop

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,15 +12,16 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, min_index;
 	int temp;
 
+	if(!array || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		min_index = i;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
-			{
 				min_index = j;
-			}
 		}
 		if (min_index != i)
 		{
@@ -30,11 +31,7 @@ void selection_sort(int *array, size_t size)
 			array[min_index] = temp;
 
 			/* Print the array after the swap */
-			for (j = 0; j < size; j++)
-			{
-				printf("%d ", array[j]);
-			}
-			printf("\n");
+			print_array(array, size);
 		}
 	}
 }
